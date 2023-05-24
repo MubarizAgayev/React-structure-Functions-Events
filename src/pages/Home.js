fe329a642003566932b4a8997e87fb91f0cc242d
mubarizra@code.edu.az <mubarizra@code.edu.az>
2023-05-24 07:36:38 +0400

    Task

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../assets/home/home.scss'
+import Header from '../components/layout/Header'
+import Main from '../components/Main'
+
+
+
+
+
+
+function Home() {
+  return (
+    <>
+    <div>
+    <header>
+    <Header/>
+    </header>
+    <main>
+      <Main/>
+    </main>
+    <footer>
+    </footer>
+    </div>
+
+    </>
+  )
+}
+
+export default Home
